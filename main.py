967868c7417c71bde300e1bdf6f00131df708de8
Alexander Lyons <alexander.n.lyons@gmail.com>
2021-11-18 08:01:35 -0500

    chore: clean up last of code

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from AoC2020.day_1 import find_sum_pair, find_sum_triplets
+
+
 def main():
-    print('This is the main function')
+    print('Welcome to Day 1 of Advent of Code 2020')
+    print('Reading data file and converting...')
+
+    data = []
+    with open('./data/day1.txt') as f:
+        for line in f:
+            data.append(int(line))
+
+    pair = find_sum_pair(2020, data)
+    print('Found pair %d' % pair)
+
+    triplets = find_sum_triplets(2020, data)
+    print('Found triplets %d' % triplets)
+
 
 if __name__ == '__main__':
     main()
